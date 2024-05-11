0fc9ee68939546e1c4fe1e19ed7d7fa5e0729621
FelixMillon <felix.millon@outlook.com>
2024-05-11 22:27:38 +0200

    instable

--- consumer copy.py
+++ consumer copy.py
@@ -1,26 +1,15 @@
-from flask import Flask, jsonify
-from flask_socketio import SocketIO, emit
-from flask_cors import CORS
 import pika
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="http://localhost:8080")
-CORS(app)
 
 # Configuration RabbitMQ
 rabbitmq_credentials = pika.PlainCredentials('rabbitMQ', 'password@123')
 rabbitmq_parameters = pika.ConnectionParameters('localhost', credentials=rabbitmq_credentials)
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('get_messages')
+@app.route('/messages', methods=['GET'])
 def get_messages():
+
     # Se connecter à RabbitMQ avec les identifiants
     connection = pika.BlockingConnection(rabbitmq_parameters)
     channel = connection.channel()
@@ -31,8 +20,8 @@ def get_messages():
     # Fonction callback pour la consommation des messages
     def callback(ch, method, properties, body):
         print("Message reçu:", body.decode())
-        # Envoyer le message au client via WebSocket
-        emit('message', body.decode())
+        return body.decode()
+        # Vous pouvez faire ce que vous voulez avec le message ici
 
     # Consommer les messages
     channel.basic_consume(queue='d118b99a-9e74-44bd-b1c0-f7a7e017e1fd', on_message_callback=callback, auto_ack=True)
@@ -41,5 +30,9 @@ def get_messages():
     print("En attente de messages...")
     channel.start_consuming()
 
+    # Réponse de l'API
+    return jsonify({"message": "Subscription started"})
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
+
--- rabbit-front/src/components/LoginComponent.vue
+++ rabbit-front/src/components/LoginComponent.vue
@@ -1,8 +1,37 @@
 <template>
-    <div>
-      <input type="text" v-model="login" placeholder="Login">
-      <input type="mdp" v-model="mdp" placeholder="Mot de passe">
-      <button @click="handleLogin">Connexion</button>
-    </div>
-  </template>
-  
+  <div>
+    <ul>
+      <li v-for="message in messages" :key="message">{{ message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+  data() {
+    return {
+      messages: [],
+      events:[],
+    };
+  },
+  created() {
+    // Se connecter au serveur Flask via WebSocket en passant par /consumer
+    this.socket = io('http://localhost:5000', { path: '/socket.io' });
+
+    // Écouter les messages envoyés par le serveur Flask
+    this.socket.on('message', (message) => {
+      this.messages.push(message);
+      console.log(message)
+    });
+    this.socket.onAny((eventName, ...args) => {
+      console.log(args)
+      console.log(eventName)
+      this.events.push(eventName);
+    });
+    // Demander au serveur Flask d'envoyer les messages
+    this.socket.emit('get_messages');
+  }
+};
+</script>
