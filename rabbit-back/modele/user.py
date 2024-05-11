76b8736b7e34601df221c8d352a075f6b825d6ff
FelixMillon <felix.millon@outlook.com>
2024-05-11 20:45:08 +0200

    bdd and user api

---
+++ rabbit-back/modele/user.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+class User:
+    def __init__(self,id,name,password):
+        self.id = id
+        self.name = name
+        self.password = password
+
+    def create_user(self,name,password):
+        
+        try:
+            conn = sqlite3.connect('user.db')
+
+            cursor = conn.cursor()
+
+            cursor.execute('''INSERT INTO users (name, password) VALUES (?, ?)''', (name, password))
+
+            new_id = cursor.lastrowid
+
+            conn.commit()
+            conn.close()
+            return {
+                "code": 201,
+                "message": "User created",
+                "id": new_id,
+                "success": True
+            }
+        except:
+            return {
+                "code": 500,
+                "message": "Error while creating user",
+                "success": False
+            }
+
+    def select_user(self,id):
+
+        conn = sqlite3.connect('user.db')
+
+        cursor = conn.cursor()
+        cursor.execute('''SELECT * FROM users where id = ?''', (id,))
+        rows = cursor.fetchall()
+        print(rows)
+        return {
+            "code": 200,
+            "message": "User selectd",
+            "user": rows[0],
+            "success": True
+        }
---
+++ rabbit-back/routers/user.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, jsonify
+from controleur.user import create_user, select_user
+user_blueprint = Blueprint("user", __name__)
+
+@user_blueprint.route("/user/", methods=["GET"])
+def send_ping_user():
+    return "ok"
+
+@user_blueprint.route("/user/", methods=["POST"])
+def send_create_user():
+    user_data = request.json
+    results = create_user(user_data)
+    return jsonify(results)
+
+@user_blueprint.route("/user/<id>/", methods=["GET"])
+def send_select_user(id):
+    results = select_user(id)
+    return jsonify(results)
